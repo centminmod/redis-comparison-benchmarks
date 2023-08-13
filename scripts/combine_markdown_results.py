14b1d5ceb5c7fb625e920d07b50771f34f6c4d65
George Liu (eva2000) <eva2000@centminmod.com>
2023-08-13 19:30:38 +1000

    update combine markdown script

--- scripts/combine_markdown_results.py
+++ scripts/combine_markdown_results.py
@@ -10,17 +10,19 @@ def combine_markdown_files(filenames, prefix):
         # Adjust the first label in the header
         header_parts = lines[0].split("|")
         header_parts[1] = " Databases "
-        combined_rows.append("|".join(header_parts))  # Adjusted header
-        combined_rows.append(lines[1])  # Separator
+        adjusted_header = "|".join(header_parts)
+        separator = lines[1]
+        
+        combined_rows.append(adjusted_header)  # Adjusted header
+        combined_rows.append(separator)  # Separator
 
     # Extract body from each file
     for idx, filename in enumerate(filenames):
         with open(filename, 'r') as file:
             lines = file.readlines()
-            if idx == 0:  # For the first file, we've already appended the header and separator
-                combined_rows.extend(lines[2:])
-            else:  # For subsequent files, skip the header and separator
-                combined_rows.extend(lines[2:])
+            for line in lines[2:]:
+                if line != adjusted_header and line != separator:
+                    combined_rows.append(line)
 
     # Write the combined results to a new markdown file with the provided prefix
     output_file = f"./benchmarklogs/combined_{prefix}_results.md"
